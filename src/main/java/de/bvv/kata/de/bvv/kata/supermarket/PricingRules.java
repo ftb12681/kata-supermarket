68b206fc13523a6c699e49338dfec37d5ce622f4
Fabian Tan <fabiantanberlin@gmail.com>
2020-10-18 15:10:10 +0200

    Implement checkConsistency() with null check

--- src/main/java/de/bvv/kata/de/bvv/kata/supermarket/PricingRules.java
+++ src/main/java/de/bvv/kata/de/bvv/kata/supermarket/PricingRules.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 /**
  * Die Klasse implementiert Funktionalitäten auf Basis des Interfaces
- * {@link PricingRulesInterface}. Preisregeln für Artikel gibt es mit 
- * unterschiedlicher Rabattierung für verschiedene Gebindegrößen. Die 
- * Klasse überprüft diese Preisregeln und kann sie für bestimmte Artikel
- * in der Weise anwenden, dass der geringstmögliche Preis entsteht.
+ * {@link PricingRulesInterface}. Preisregeln für Artikel gibt es mit
+ * unterschiedlicher Rabattierung für verschiedene Gebindegrößen. Die Klasse
+ * überprüft diese Preisregeln und kann sie für bestimmte Artikel in der Weise
+ * anwenden, dass der geringstmögliche Preis entsteht.
  * 
  * @author bvv/b00359 (Tobias Zepter)
  */
@@ -19,30 +19,35 @@ public class PricingRules implements PricingRulesInterface {
 	public void init(List<PricingRuleValueObject> pricingRulesList) {
 		this.pricingRulesList = pricingRulesList;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public boolean isPriceAvailableFor(char articleName) {
-		//...
+		// ...
 		return false;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public InterimResult getPriceForNEqualItems(char articleName, int articleCount) {
 		InterimResult nullReturner = new InterimResult(0.0d, articleCount);
-		//...
+		// ...
 		return nullReturner;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void checkConsistency() throws IllegalStateException {
-		throw new IllegalStateException("nix implementiert");
+		if (null == pricingRulesList)
+			throw new IllegalStateException("nix implementiert");
 	}
+
 	/**
 	 * interne Repräsentation der Preisliste
 	 */
