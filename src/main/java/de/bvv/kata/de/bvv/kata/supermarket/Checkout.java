b534dc270bf1fdf99246caf42e9af5c7fabe73ad
Fabian Tan <fabiantanberlin@gmail.com>
2020-10-18 15:27:15 +0200

    Add scannedArticles field and implement scan()

--- src/main/java/de/bvv/kata/de/bvv/kata/supermarket/Checkout.java
+++ src/main/java/de/bvv/kata/de/bvv/kata/supermarket/Checkout.java
@@ -1,13 +1,16 @@
 package de.bvv.kata.de.bvv.kata.supermarket;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Diese Klasse stellt die Funktionalität der Supermarkt-Kasse. Sie
- * kann mit Preisregeln "gefüttert" werden, scannt seriell einen Artikel nach dem
- * anderen und kann zu jedem Zeitpunkt Auskunft über den minimalen aktuellen
- * Preis der bereits gescannten Artikel geben. Zur Vereinfachung
- * werden die bereits gescannten Artikel nach Abrechnung nicht gelöscht, 
- * sondern für den Scan-Vorgang eines neuen Warenkorbes einfach eine neue
- * Instanz der Klasse gebildet.
+ * Diese Klasse stellt die Funktionalität der Supermarkt-Kasse. Sie kann mit
+ * Preisregeln "gefüttert" werden, scannt seriell einen Artikel nach dem anderen
+ * und kann zu jedem Zeitpunkt Auskunft über den minimalen aktuellen Preis der
+ * bereits gescannten Artikel geben. Zur Vereinfachung werden die bereits
+ * gescannten Artikel nach Abrechnung nicht gelöscht, sondern für den
+ * Scan-Vorgang eines neuen Warenkorbes einfach eine neue Instanz der Klasse
+ * gebildet.
  * 
  * @author bvv\b00359 (Tobias Zepter)
  */
@@ -20,24 +23,42 @@ public class Checkout implements CheckoutInterface {
 		pricingRules.checkConsistency();
 		this.pricingRules = pricingRules;
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void scan(char articleName) throws IllegalArgumentException {
-		//...
+		if (null == pricingRules)
+			throw new IllegalStateException("PricingRules are missing");
+
+		if (!pricingRules.isPriceAvailableFor(articleName))
+			throw new IllegalArgumentException(String.format("No price available for articleName %s", articleName));
+		if (scannedArticles.containsKey(articleName))
+			scannedArticles.put(articleName, 1 + scannedArticles.get(articleName));
+		else
+			scannedArticles.put(articleName, 1);
 	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public double getTotalPrice() throws IllegalStateException {
 		double priceToReturn = 0d;
-		//...
+		if (null == this.pricingRules)
+			throw new IllegalStateException("PricingRules are missing");
 		return priceToReturn;
 	}
+
 	/**
 	 * interne Datenhaltung für die aktuellen Preisregeln
 	 */
 	PricingRulesInterface pricingRules;
+
+	/**
+	 * interne Datenhaltung für gescannte Artikel
+	 */
+	Map<Character, Integer> scannedArticles = new HashMap<Character, Integer>();
+
 }
